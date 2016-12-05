e0e3e226c93dd00bdaabdf36c2ac3d37490f4e1b
PeterPan-PC\Peter Pan <51656281@qq.com>
2016-12-06 00:06:20 +0800

    上行70%

--- elevator.js
+++ elevator.js
@@ -7,14 +7,10 @@ var Elevator = function (index,high,basecoat,init){
     this.up = new Array();
     //下行最大位置
     this.down = new Array();
-    //目标位置
-    this.target = 0;
-    //新目标
-    this.newTarget = 0;
     //当前位置
     this.dt = 1;
     //state 状态 静止：0 上行：1 下行：2
-    this.state = 4;
+    this.state = 0;
     //电梯index
     this.index = index;
     //电梯作用范围 high:作用最高楼层 basecoat:作用最低楼层 init:初始楼层
@@ -28,126 +24,111 @@ var Elevator = function (index,high,basecoat,init){
 Elevator.prototype = {
     //移动逻辑
     judge: function(){
-        if(this.state == 0){
-
-        }else if(this.state == 1){
-
-        }else if(this.state == 2){
-
-        }
-
+        var self = this;
+        this.up = new Array();
+        this.down = new Array();
+        $(".dt li").each(function(){
+            if($(this).find('.btn-move').find('.put-up').attr('data-type') == 1){
+                var tag_up = $(this).data('tag');
+                self.up.push(tag_up);
+            }
+            if($(this).find('.btn-move').find('.put-down').attr('data-type') == 1){
+                var tag_down = $(this).data('tag');
+                self.down.push(tag_down);
+            }
+        });
         if(this.controlWork == 0) {
-            var self = this;
+            self.controlWork = 1;
             this.moveControl = setInterval(function () {
                 self.move();
-                this.controlWork = 1;
             }, 1000);
         }
+
     },
 
    move:function(){
-       var str = "target:" + this.target + ",newTarget:" + this.newTarget + ",dt:" + this.dt + ",state:"+this.state;
-       console.log(str);
-       if(this.target == this.dt || this.target == 0){
-           //到达目标，判断下一目标
-           if(this.newTarget == 0){
-               //无下一目标
-               clearInterval(this.moveControl);
-               this.controlWork = 0;
-               return false;
+       var self = this;
+       console.log(this.up);
+       console.log(this.down);
+       if(this.state == 1){
+           if(this.up.length == 0){
+               //上行为空
+               this.state = 0;
+           }else{
+               if(this.up[0] > this.dt){
+                   //上行未走完
+                   this.putUp();
+               }else{
+                   //上行走完
+                   this.state = 0;
+               }
            }
-           else{
-               //更新目标
-               this.target = this.newTarget;
-               this.newTarget = 0;
+       }
+       if(this.state == 2){
+           if(this.down.length == 0){
+               //下行为空
+               this.state = 0;
+           }else{
+               if(this.down[this.down.length - 1] < this.dt){
+                   //下行未走完
+                   this.putUp();
+               }else{
+                   //下行走完
+                   this.state = 0;
+               }
            }
        }
-
-       if((this.target - this.dt) == 0){
-           //bug
-           alert(this.target);
-       }
-       else if((this.target - this.dt) > 0){
-           //上行
-           this.state = 1;
-           this.putUp();
-       }
-       else if((this.target - this.dt) < 0){
-           //下行
-           this.state = 2;
-           this.putDown();
-       }
-
-
-
-
-       //if(this.state == 0){
-       //    if(this.target == 0 || this.target == this.dt){
-       //        clearInterval(this.moveControl);
-       //        this.state = 4;
-       //        return false;
-       //    }
-       //}
-       //
-       //if(this.state == 0){
-       //    if(this.up == this.down == this.dt || (this.up == 0 && this.down == 0)){
-       //        this.state = 0;
-       //        clearInterval(this.moveControl);
-       //        this.state = 4;
-       //        return false;
-       //    }
-       //    else if(this.up != this.dt || this.down != this.dt){
-       //         if(this.up == 0){
-       //             //下行
-       //             this.state = 2;
-       //             this.putDown();
-       //         }else if(this.down == 0){
-       //             //上行
-       //             this.state = 1;
-       //             this.putUp();
-       //         }else{
-       //             var goup = this.up - this.dt;
-       //             var godown = this.dt - this.down;
-       //             if((goup - godown) > 0){
-       //                 //下行
-       //                 this.state = 2;
-       //                 this.putDown();
-       //             }else{
-       //                 //上行
-       //                 this.state = 1;
-       //                 this.putUp();
-       //             }
-       //         }
-       //    }
-       //    else{
-       //        clearInterval(this.moveControl);
-       //        var str = "up:" + this.up + ",down:" + this.down + ",dt:" + this.dt + ",state:"+this.state;
-       //        alert(str);
-       //        return false;
-       //    }
-       //}
-       //else if(this.state == 1){
-       //    if(this.up == this.dt){
-       //        //上行结束
-       //        this.state = 0;
-       //        this.up = 0;
-       //    }
-       //    else{
-       //        //上行未结束，继续上行
-       //        this.putUp();
-       //    }
-       //}
-       //else if(this.state == 2){
-       //    if(this.down == this.dt){
-       //        //下行结束
-       //        this.state = 0;
-       //        this.down = 0;
-       //    }
-       //    else{
-       //        //下行未结束,继续下行
-       //        this.putDown();
-       //    }
-       //}
+        if(this.state == 0){
+            if(this.up.length == 0 && this.down.length == 0){
+                //所有行动结束
+                clearInterval(this.moveControl);
+                this.controlWork = 0;
+                return false;
+            }
+            else{
+                //不能判断是否上下行
+                /*
+                先判断上行，再判断下行
+                上行就近
+                 */
+                //判断上行
+                var up_state = self.dt;
+                var up_state_d = self.dt;
+                var down_state = self.dt;
+                var down_state_u = self.dt;
+                if(this.up.length != 0){
+                    $.each(this.up,function(index){
+                        if(this > up_state){
+                            up_state = this;
+                        }
+                        else if(this < up_state_d){
+                            up_state_d = this;
+                        }
+                    })
+                }
+                if(this.down.length != 0){
+                    $.each(this.down,function(index){
+                        if(this > down_state){
+                            down_state = this;
+                        }
+                        else if(this < down_state_u){
+                            down_state_u = this;
+                        }
+                    })
+                }
+                up_state = (up_state - this.dt) < (down_state_u - this.dt) ? (up_state - this.dt) : (down_state_u - this.dt);
+                down_state = (this.dt + down_state) < (this.dt - up_state_d) ? (this.dt + down_state) : (this.dt - up_state_d);
+                if(up_state < down_state){
+                    //下行
+                    this.state = 2;
+                    this.putDown();
+                }else{
+                    //上行
+                    this.state = 1;
+                    this.putUp();
+                }
+            }
+        }
    },
 
     putUp:function(){
@@ -156,18 +137,24 @@ Elevator.prototype = {
             if($(this).data('tag') == dt){
                 $(this).find('.dt-1').removeClass('glyphicon');
                 $(this).find('.dt-1').removeClass('glyphicon-asterisk');
-
             }
             if($(this).data('tag') == (dt + 1)){
                 $(this).find('.dt-1').addClass('glyphicon');
                 $(this).find('.dt-1').addClass('glyphicon-asterisk');
                 $(this).find('.btn-move').find('.put-up').removeClass('orange');
+                $(this).find('.btn-move').find('.put-up').attr('data-type', '0');
                 if((dt + 1) == 10){
                     $(this).find('.btn-move').find('.put-down').removeClass('orange');
                 }
             }
         });
         this.dt = dt + 1;
+        var self = this;
+        $.each(this.up,function(index){
+            if(this == (dt+1)){
+                self.up.splice(index,1);
+            }
+        })
     },
 
     putDown:function(){
@@ -181,11 +168,18 @@ Elevator.prototype = {
                 $(this).find('.dt-1').addClass('glyphicon');
                 $(this).find('.dt-1').addClass('glyphicon-asterisk');
                 $(this).find('.btn-move').find('.put-down').removeClass('orange');
+                $(this).find('.btn-move').find('.put-down').attr('data-type', '0');
                 if((dt - 1) == 1){
                     $(this).find('.btn-move').find('.put-up').removeClass('orange');
                 }
             }
         });
         this.dt = dt - 1;
+        var self = this;
+        $.each(this.down,function(index){
+            if(this == (dt+1)){
+                self.down.splice(index,1);
+            }
+        })
     }
 };
